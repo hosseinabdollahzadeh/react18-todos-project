f830218166e8bd0224710c66a4da988cdf1eff83
technosun <abdollahzadeh.hossein@gmail.com>
2024-10-20 16:21:15 +0330

    Show todos

--- src/pages/Todos.js
+++ src/pages/Todos.js
@@ -32,16 +32,30 @@ const Todos = () => {
     }, []);
     return (
         <div className="container mt-5">
-            <div className="row">
+            <div className="row g-3">
                 {error && <div>{error}</div>}
                 {loading && <div className="col-md-12 text-center">
                     <div className="spinner-border mt-5"></div>
                 </div>}
                 {todos && todos.map(todo => (
-                    <h1 key={todo.id}>{todo.title}</h1>
+                    <div className="col-md-4" key={todo.id}>
+                        <div className={"card " + (todo.completed && "bg-light")}>
+                            <div className="card-body d-flex justify-content-between align-items-center">
+                                <div>{todo.completed ? <del>{todo.title}</del> : <span>{todo.title}</span>}</div>
+                                <div className="d-flex align-items-center">
+                                    {todo.completed ?
+                                        <i className="bi bi-check-all fs-4"></i>
+                                        :
+                                        <i className="bi bi-check fs-4"></i>
+                                    }
+
+                                    <i className="bi bi-trash-fill fs-6"></i>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 ))}
             </div>
-
         </div>
     )
 }
